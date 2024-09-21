6723303a8edba56a61ece268dabb0d29c2f21c4b
at2005 <tambdeayush@gmail.com>
2024-09-21 11:38:27 +0000

    faster

--- src/sim.cuh
+++ src/sim.cuh
@@ -60,18 +60,18 @@ __device__ double danby_burkardt(double mean_anomaly, double eccentricity)
     sincos(E, &sin_E, &cos_E);
     double e_sin = eccentricity * sin_E;
     double f = E - e_sin - mean_anomaly;
-    if (fabs(f) < CUTOFF) break;
     double e_cos = eccentricity * cos_E;
     double f_prime = 1 - e_cos;
     double dE = -f / f_prime;
     dE = -f / (f_prime + dE * e_sin / 2.00);
     // higher order convergence only near the end
-    if i > MAX_ITERATIONS_ROOT_FINDING - 3 {
+    if (i > MAX_ITERATIONS_ROOT_FINDING - 3)
+    {
       // quartic convergence
       dE = -f / ((f_prime + dE * e_sin / 2.00) + (dE * dE * e_cos / 6.00));
       // quintic convergence
       dE = -f / ((f_prime + dE * e_sin / 2.00) + (dE * dE * e_cos / 6.00) -
-                (dE * dE * dE * e_sin / 24.00));
+                 (dE * dE * dE * e_sin / 24.00));
     }
     E += dE;
   }
